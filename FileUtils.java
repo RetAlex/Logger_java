401dcea6075fc62e6046ce68134d5f33f99f2d82
Oleksii <aar97@ukr.net>
2018-10-16 21:46:43 +0300

    Added additional function to FileUtils

--- FileUtils.java
+++ FileUtils.java
@@ -20,6 +20,19 @@ public class FileUtils {
         }
     }
 
+    public static void writeToFile(String path, String message, boolean append) throws Exception {
+        File f = new File(path);
+        if(!f.exists()) {
+            f.getParentFile().mkdirs();
+            f.createNewFile();
+        }
+        try (FileWriter fw = new FileWriter(f, append)) {
+            fw.write(message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static String loadAsString(String path){
         try {
             File yourFile = new File(path);
